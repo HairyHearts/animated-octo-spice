696fa458c2285d63d4fcc9224c7363898f246591
Alessandro Mancini <alem1979@gmail.com>
2014-02-08 21:32:11 +0100

    integrato il datastore

--- src/org/hairyhearts/GCMService.java
+++ src/org/hairyhearts/GCMService.java
@@ -5,14 +5,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.baasbox.android.BaasDocument;
+import com.baasbox.android.BaasException;
+import com.baasbox.android.BaasHandler;
+import com.baasbox.android.BaasResult;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 public class GCMService extends IntentService {
+    private static final String TAG = "GCMService";
 
     public GCMService() {
         super("GCMService");
 
-        Log.d("GCMService", "");
     }
 
     @Override
@@ -20,11 +24,26 @@ public class GCMService extends IntentService {
         Bundle extras = intent.getExtras();
         GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
         String messageType = gcm.getMessageType(intent);
+
         if (!extras.isEmpty()) {
             if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
-                Log.d("RECEIVED", "MESSAGE: " + extras.toString());
+                String id = extras.getString("message");
+
+                BaasResult<BaasDocument> res = BaasDocument.fetchSync("message", id);
+
+                BaasDocument doc;
+                try {
+                    doc = res.get();
+
+                    Log.d(TAG, "from: " + doc.getString("from"));
+                    Log.d(TAG, "to: " + doc.getString("to"));
+                    Log.d(TAG, "msg: " + doc.getString("msg"));
+                } catch (BaasException e) {
+                    e.printStackTrace();
+                }
             }
         }
+
         GCMReceiver.completeWakefulIntent(intent);
     }
 }
--- src/org/hairyhearts/SuccessActivity.java
+++ src/org/hairyhearts/SuccessActivity.java
@@ -1,6 +1,11 @@
 package org.hairyhearts;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.media.audiofx.BassBoost;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -8,9 +13,12 @@ import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.baasbox.android.BaasBox;
+import com.baasbox.android.BaasDocument;
 import com.baasbox.android.BaasHandler;
 import com.baasbox.android.BaasResult;
 import com.baasbox.android.BaasUser;
+import com.baasbox.android.SaveMode;
 import com.baasbox.android.json.JsonObject;
 
 public class SuccessActivity extends Activity {
@@ -29,29 +37,43 @@ public class SuccessActivity extends Activity {
     }
 
     private void sendMessage() {
+        String from = BaasUser.current().getName();
         String to = ((EditText) findViewById(R.id.to)).getText().toString();
         String msg = ((EditText) findViewById(R.id.message)).getText().toString();
 
-        BaasUser.withUserName(to).send(new JsonObject().putString("message", msg), new BaasHandler<Void>() {
-            @Override
-            public void handle(BaasResult<Void> voidBaasResult) {
-                Log.d("RESULT: ", voidBaasResult.toString());
-
-                Toast.makeText(SuccessActivity.this, "SENT MESSAGE", Toast.LENGTH_LONG).show();
-            }
-        });
+        new SendMessage(from, to, msg).execute();
     }
 
-    // private void sendMessage() {
-    // BaasUser.withUserName("ooooo").send(new JsonObject().putString("message",
-    // "ciao"), new BaasHandler<Void>() {
-    // @Override
-    // public void handle(BaasResult<Void> voidBaasResult) {
-    // Log.d("RESULT: ", voidBaasResult.toString() );
-    //
-    // Toast.makeText(SuccessActivity.this, "SENT MESSAGE",
-    // Toast.LENGTH_LONG).show();
-    // }
-    // });
-    // }
+    public class SendMessage extends AsyncTask<Void, Void, String> {
+        private String from;
+        private String to;
+        private String msg;
+
+        SendMessage(String from, String to, String msg) {
+            this.from = from;
+            this.to = to;
+            this.msg = msg;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            BaasDocument doc = new BaasDocument("message");
+            doc.putString("from", from);
+            doc.putString("to", to);
+            doc.putString("msg", msg);
+            doc.saveSync(SaveMode.IGNORE_VERSION);
+
+            return doc.getId();
+        }
+
+        @Override
+        protected void onPostExecute(String id) {
+            BaasUser.withUserName(to).send(new JsonObject().putString("message", id), new BaasHandler<Void>() {
+                @Override
+                public void handle(BaasResult<Void> voidBaasResult) {
+                    Toast.makeText(SuccessActivity.this, "Sent message", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
 }
